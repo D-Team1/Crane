52b64d63c8fd46fe0d9555ff63fad9c25070c23e
Keitaro0408 <kei.kotani77@gmail.com>
2017-10-02 20:04:49 +0900

    作業内容
    - オブジェクトがひっつくようにした

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0,0,0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Claw")
+        {
+            gameObject.transform.position = other.transform.position;
+        }
+    }
+}
