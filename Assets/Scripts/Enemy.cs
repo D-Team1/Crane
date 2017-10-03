a6042fa438baf0d7e4dbd9bb17f99859b7854ba6
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-10-03 11:31:35 +0900

    敵の移動の作成

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵クラス
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    float speed = 0.5f;
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// 敵の死亡時に呼び出す
+    /// </summary>
+    void destroy()
+    {
+        //TODO: 死亡時の処理
+    }
+}
