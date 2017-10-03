7829e361323b8e9b66db201dfb55365ee5d1b4ab
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-10-03 12:02:29 +0900

    Merge branch 'Arm' of https://github.com/D-Team1/Crane into develop
    # Conflicts:
    #	Assets/Scenes/Kotani.unity

---
+++ Assets/Scripts/Main/ArmControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmControl : MonoBehaviour {
+
+    private Vector3 m_Pos;
+    private bool m_IsMove;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_IsMove = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-0.05f,0f,0f); //形状位置を更新
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(+0.05f, 0f, 0f); //形状位置を更新
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) && m_IsMove)
+        {
+            transform.position += new Vector3(0, -0.05f, 0f); //形状位置を更新
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, +0.05f, 0f); //形状位置を更新
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        m_IsMove = false;
+        rb.isKinematic = true;
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        m_IsMove = false;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        m_IsMove = true;
+    }
+}
---
+++ Assets/Scripts/Main/BlockControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockControl : MonoBehaviour {
+
+    private bool m_IsCaught; // 掴まれているか
+    private float m_Time;
+    private bool m_IsMove;
+    private GameObject m_Child;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0,0,0);
+        m_IsCaught = false;
+        m_Child = transform.Find("Cube").gameObject;
+        m_Time = 0f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(m_IsCaught)
+        {
+            GameObject ClawObject = GameObject.FindGameObjectWithTag("Claw");
+            gameObject.transform.position = ClawObject.transform.position;
+        }
+
+        if (!m_IsCaught)
+        {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+
+            GetComponent<Rigidbody>().useGravity = true;
+            m_Child.GetComponent<Rigidbody>().useGravity = true;
+            m_Time += Time.deltaTime;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            m_Child.GetComponent<Rigidbody>().useGravity = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_IsCaught = false;
+            m_Time = 0;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Claw" &&
+            m_Time > 0.5f)
+        {
+            m_IsCaught = true;
+            gameObject.transform.position = other.transform.position;
+
+            GetComponent<Rigidbody>().useGravity = false;
+            m_Child.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+}
