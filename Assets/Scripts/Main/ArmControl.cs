9bee5a876e5abc01b09d054503690daecdb1b1c5
Keitaro0408 <kei.kotani77@gmail.com>
2017-10-03 11:51:52 +0900

    作業内容
    - アームでブロックを押せないようにした。

--- Assets/Scripts/Main/ArmControl.cs
+++ Assets/Scripts/Main/ArmControl.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestMove : MonoBehaviour {
+public class ArmControl : MonoBehaviour {
 
     private Vector3 m_Pos;
-	// Use this for initialization
-	void Start ()
+    private bool m_IsMove;
+
+    // Use this for initialization
+    void Start ()
     {
+        m_IsMove = true;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-0.05f,0f,0f); //形状位置を更新
@@ -22,8 +26,7 @@ public class TestMove : MonoBehaviour {
             transform.position += new Vector3(+0.05f, 0f, 0f); //形状位置を更新
         }
 
-        if (Input.GetKey(KeyCode.DownArrow) &&
-            gameObject.transform.position.y > 3.5f)
+        if (Input.GetKey(KeyCode.DownArrow) && m_IsMove)
         {
             transform.position += new Vector3(0, -0.05f, 0f); //形状位置を更新
         }
@@ -31,5 +34,22 @@ public class TestMove : MonoBehaviour {
         {
             transform.position += new Vector3(0, +0.05f, 0f); //形状位置を更新
         }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        m_IsMove = false;
+        rb.isKinematic = true;
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        m_IsMove = false;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        m_IsMove = true;
     }
 }
--- Assets/Scripts/Main/BlockControl.cs
+++ Assets/Scripts/Main/BlockControl.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class BlockControl : MonoBehaviour {
 
     private bool m_IsCaught; // 掴まれているか
     private float m_Time;
+    private bool m_IsMove;
     private GameObject m_Child;
 
 	// Use this for initialization
@@ -13,6 +14,7 @@ public class Test : MonoBehaviour {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.velocity = new Vector3(0,0,0);
         m_IsCaught = false;
+        m_Child = transform.FindChild("Cube").gameObject;
         m_Time = 0f;
     }
 	
@@ -27,15 +29,16 @@ public class Test : MonoBehaviour {
 
         if (!m_IsCaught)
         {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+
             GetComponent<Rigidbody>().useGravity = true;
-            m_Child = transform.FindChild("Cube").gameObject;
             m_Child.GetComponent<Rigidbody>().useGravity = true;
             m_Time += Time.deltaTime;
         }
         else
         {
             GetComponent<Rigidbody>().useGravity = false;
-            m_Child = transform.FindChild("Cube").gameObject;
             m_Child.GetComponent<Rigidbody>().useGravity = false;
         }
 
@@ -54,9 +57,14 @@ public class Test : MonoBehaviour {
             m_IsCaught = true;
             gameObject.transform.position = other.transform.position;
 
-            m_Child = transform.FindChild("Cube").gameObject;
             GetComponent<Rigidbody>().useGravity = false;
             m_Child.GetComponent<Rigidbody>().useGravity = false;
         }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 }
