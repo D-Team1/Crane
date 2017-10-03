c1f4389143eb9d8ed7e183df81ec8c9de489a943
DESKTOP-38QN5D3\dawa <dawa31819625@gmail.com>
2017-10-03 17:07:51 +0900

    キャラクターの挙動実装

---
+++ Assets/Script/BurikiMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurikiMove : MonoBehaviour
+{
+
+    Rigidbody rd;
+    public int MoveSpeed = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //velocity::速度
+        //x方向へMoveSpeed分移動させる
+        rd.velocity = new Vector2(MoveSpeed, rd.velocity.y);
+    }
+}
+
+
+   
+
+ 
---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject Buriki;
+   
+    // Use this for initialization
+    void Start () {
+        this.Buriki = GameObject.Find("Buriki");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 BurikiPos = this.Buriki.transform.position;
+        transform.position = new Vector3(BurikiPos.x,BurikiPos.y,transform.position.z);
+    }
+}
