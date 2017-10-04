70fc360ecbd735659a6c03cf0fc20d650ba6275f
Keitaro0408 <kei.kotani77@gmail.com>
2017-10-04 19:37:46 +0900

    キャラクターのモデルの入れ替え

--- Assets/Scripts/BurikiMove.cs
+++ Assets/Scripts/BurikiMove.cs
@@ -7,12 +7,14 @@ public class BurikiMove : MonoBehaviour
 
     Rigidbody rd;
     public float MoveSpeed = 0.5f;
+    private Animator anim;
 
     // Use this for initialization
     void Start()
     {
         //GetComponentの処理をキャッシュしておく
         rd = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +23,10 @@ public class BurikiMove : MonoBehaviour
     public float z = 0;
     void FixedUpdate()
     {
-        //velocity::速度
-        //x方向へMoveSpeed分移動させる
+        
         transform.position += new Vector3(0.01f, 0f, 0f);
+       
+        //anim.SetFloat("speed");
     }
 
 
