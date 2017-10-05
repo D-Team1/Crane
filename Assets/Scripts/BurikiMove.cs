523a2edf0495a0f9ee7acb2acbb93618ca4fd48a
Keitaro0408 <kei.kotani77@gmail.com>
2017-10-05 10:16:08 +0900

    ロボットの当たり判定修正

--- Assets/Scripts/BurikiMove.cs
+++ Assets/Scripts/BurikiMove.cs
@@ -7,6 +7,7 @@ public class BurikiMove : MonoBehaviour
 
     Rigidbody rd;
     public float MoveSpeed = 0.5f;
+    public GameObject DestroyEffect;
     private Animator anim;
 
     // Use this for initialization
@@ -23,9 +24,13 @@ public class BurikiMove : MonoBehaviour
     public float z = 0;
     void FixedUpdate()
     {
-        
-        transform.position += new Vector3(0.01f, 0f, 0f);
-       
+        Vector3 rayPos = transform.position;
+        rayPos.y += 0.5f;
+        Debug.DrawRay(rayPos, Vector3.right*0.4f);
+        if(!Physics.Raycast(rayPos, Vector3.right, 0.4f))
+        {
+            transform.position += new Vector3(0.01f, 0f, 0f);
+        }
         //anim.SetFloat("speed");
     }
 
@@ -39,15 +44,19 @@ public class BurikiMove : MonoBehaviour
         }
         else if (clear.gameObject.tag == "GameOver")
         {
-            Debug.Log("gameover");
             GameManager.Instance.gameOver();
+            Instantiate(DestroyEffect,gameObject.transform.position,Quaternion.identity);
+            Destroy(gameObject);
         }
-
-        if (clear.gameObject.tag == "BuildingBlocks")
+        else if (clear.gameObject.tag == "BuildingBlocks")
         {
-            if (Physics.Raycast(transform.position, Vector3.up, 10))
+            Vector3 rayPos = transform.position;
+            rayPos.y += 1.5f;
+            Vector3 rayDir = new Vector3(0,1,0);
+            if (Physics.Raycast(rayPos, rayDir,10))
             {
                 GameManager.Instance.gameOver();
+                Instantiate(DestroyEffect, gameObject.transform.position, Quaternion.identity);
                 Destroy(gameObject);
             }
         }
